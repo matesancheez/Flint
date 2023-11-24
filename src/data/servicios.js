101c5a16d0e27691e9255ac62e1a97e8542a669b
Mateo <sanchezmateo090@gmail.com>
2023-11-24 13:59:50 -0300

    Cambiando Hero, Contacto y corrigiendo iconos

--- src/data/servicios.js
+++ src/data/servicios.js
@@ -3,109 +3,126 @@ export const servicios = [
         name: "SEO",
         description: "We'll optimize your website to rank higher in search engine results pages.",
         category: "web",
-        icon: "fas fa-search",
+        icon: '/icons/seo.png',
         isTopSales: true
     },
     {
         name: "Pay-Per-Click Advertising",
         description: "We'll create and manage PPC campaigns to drive traffic to your website.",
         category: "web",
+        icon: '/icons/web.png',
         isTopSales: false
     },
     {
         name: "Content Marketing",
         description: "We'll create and distribute valuable content to attract and retain a clearly defined audience.",
         category: "merchandise",
+        icon: '/icons/merch.png',
         isTopSales: false
     },
     {
         name: "Influencer Marketing",
         description: "We'll partner with influencers to promote your brand and reach a wider audience.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: true
     },
     {
         name: "Video Marketing",
         description: "We'll create engaging video content to promote your brand and products.",
         category: "merchandise",
+        icon: '/icons/merch.png',
         isTopSales: false
     },
     {
         name: "Event Marketing",
         description: "We'll plan and execute events to promote your brand and products.",
         category: "web",
+        icon: '/icons/web.png',
         isTopSales: false
     },
     {
         name: "Affiliate Marketing",
         description: "We'll partner with affiliates to promote your products and earn commissions on sales.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: true
     },
     {
         name: "Brand Management",
         description: "We'll help you develop and maintain a strong brand identity.",
         category: "merchandise",
+        icon: '/icons/merch.png',
         isTopSales: false
     },
     {
         name: "Market Research",
         description: "We'll conduct market research to help you better understand your target audience.",
         category: "web",
+        icon: '/icons/web.png',
         isTopSales: true
     },
     {
         name: "Public Relations",
         description: "We'll manage your public image and help you build relationships with the media.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: false
     },
     {
         name: "Web Design",
         description: "We'll design and develop a website that showcases your brand and products.",
         category: "web",
+        icon: '/icons/web.png',
         isTopSales: false
     },
     {
         name: "Graphic Design",
         description: "We'll create visually appealing graphics to promote your brand and products.",
         category: "merchandise",
+        icon: '/icons/merch.png',
         isTopSales: true
     },
     {
         name: "Branded Merchandise",
         description: "We'll create branded merchandise to help promote your brand and products.",
         category: "merchandise",
+        icon: '/icons/merch.png',
         isTopSales: false
     },
     {
         name: "Mobile Marketing",
         description: "We'll create and manage mobile marketing campaigns to reach your target audience on their devices.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: false
     },
     {
         name: "Direct Mail Marketing",
         description: "We'll create and send direct mail campaigns to your target audience.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: false
     },
     {
         name: "Podcast Advertising",
         description: "We'll create and manage podcast advertising campaigns to reach your target audience.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: false
     },
     {
         name: "SMS Marketing",
         description: "We'll create and manage SMS marketing campaigns to reach your target audience on their phones.",
         category: "marketing",
+        icon: '/icons/marketing.png',
         isTopSales: false
     },
     {
         name: "In-Store Promotions",
         description: "We'll create and execute in-store promotions to drive traffic to your physical locations.",
         category: "web",
+        icon: '/icons/web.png',
         isTopSales: true
     }
 ];
