c7d0b7e65079d970593126e9b0a69d36c1f4c122
Mateo <sanchezmateo090@gmail.com>
2024-04-13 15:09:37 -0300

    corrigiendo path de imagenes

---
+++ src/content/posts/Creando-un-nuevo-post.md
@@ -0,0 +1,13 @@
+---
+author: Mateo
+authorimg: /images/autores/dwight.avif
+categories:
+  - insights
+date: 2024-04-13T03:00:00.000Z
+image: /images/photoshoot.jpg
+title: Creando un nuevo post
+---
+
+## Este es un nuevo post 
+
+Esta creado con un archivo markdown desde TinaCMS
--- tina/config.js
+++ tina/config.js
@@ -21,8 +21,9 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+      mediaRoot: "images",
+      publicFolder: "/",
+      
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
